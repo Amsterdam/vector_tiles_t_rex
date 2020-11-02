0602788ca05201555d746428977934b777b4fcfd
Edward Mac Gillavry <edward@webmapper.net>
2020-11-02 17:03:47 +0100

    Add KBK10 and KBK50 terrain objects.

--- config/database/mview_terreindeel_vlak.sql
+++ config/database/mview_terreindeel_vlak.sql
@@ -267,7 +267,234 @@ UNION
  	 	22 as maxzoom
    FROM bgt."BGT_BTRN_heide"
   WHERE 1=1
+
+    /* KBK10 */
+
+  UNION
+  SELECT
+		"TRN_basaltblokken_steenglooiing".ogc_fid::text || 'TRN_basaltblokken_steenglooiing_kbk10' as identificatie_lokaal_id,
+    'basaltblokken_steenglooiing' as type,
+		ST_makeValid(    "TRN_basaltblokken_steenglooiing".geom) as geometrie, 
+    0  as relatievehoogteligging, 
+ 	 	'kbk10' as bron, 
+ 	 	13  as minzoom, 
+ 	 	15 as maxzoom
+  FROM kbk10."TRN_basaltblokken_steenglooiing"
+  UNION
+  SELECT
+		"TRN_grasland".ogc_fid::text || 'TRN_grasland_kbk10' as identificatie_lokaal_id,
+    'grasland' as type,
+		ST_makeValid(    "TRN_grasland".geom) as geometrie, 
+    0  as relatievehoogteligging, 
+ 	 	'kbk10' as bron, 
+ 	 	13  as minzoom, 
+ 	 	15 as maxzoom
+  FROM kbk10."TRN_grasland"
+  UNION 
+  SELECT
+		"TRN_akkerland".ogc_fid::text || 'TRN_akkerland_kbk10' as identificatie_lokaal_id,
+    'akkerland' as type,
+		ST_makeValid(    "TRN_akkerland".geom) as geometrie, 
+    0  as relatievehoogteligging, 
+ 	 	'kbk10' as bron, 
+ 	 	13  as minzoom, 
+ 	 	15 as maxzoom
+  FROM kbk10."TRN_akkerland"
+  UNION 
+  SELECT
+		"TRN_overig".ogc_fid::text || 'TRN_overig_kbk10' as identificatie_lokaal_id,
+    'overig' as type,
+		ST_makeValid(    "TRN_overig".geom) as geometrie, 
+    0  as relatievehoogteligging, 
+ 	 	'kbk10' as bron, 
+ 	 	13  as minzoom, 
+ 	 	15 as maxzoom
+  FROM kbk10."TRN_overig"
+  UNION 
+  SELECT
+		"TRN_bedrijfsterrein".ogc_fid::text || 'TRN_bedrijfsterrein_kbk10' as identificatie_lokaal_id,
+    'bedrijfsterrein' as type,
+		ST_makeValid(    "TRN_bedrijfsterrein".geom) as geometrie, 
+    0  as relatievehoogteligging, 
+ 	 	'kbk10' as bron, 
+ 	 	13  as minzoom, 
+ 	 	15 as maxzoom
+  FROM kbk10."TRN_bedrijfsterrein"
+  UNION 
+  SELECT
+		"TRN_openbaar_groen".ogc_fid::text || 'TRN_openbaar_groen_kbk10' as identificatie_lokaal_id,
+    'openbaar_groen' as type,
+		ST_makeValid(    "TRN_openbaar_groen".geom) as geometrie, 
+    0  as relatievehoogteligging, 
+ 	 	'kbk10' as bron, 
+ 	 	13  as minzoom, 
+ 	 	15 as maxzoom
+  FROM kbk10."TRN_openbaar_groen"
+  UNION 
+  SELECT
+		"TRN_zand".ogc_fid::text || 'TRN_zand_kbk10' as identificatie_lokaal_id,
+    'zand' as type,
+		ST_makeValid(    "TRN_zand".geom) as geometrie, 
+    0  as relatievehoogteligging, 
+ 	 	'kbk10' as bron, 
+ 	 	13  as minzoom, 
+ 	 	15 as maxzoom
+  FROM kbk10."TRN_zand"
+  UNION 
+  SELECT
+		"TRN_bos-loofbos".ogc_fid::text || 'TRN_bos-loofbos_kbk10' as identificatie_lokaal_id,
+    'bos-loofbos' as type,
+		ST_makeValid(    "TRN_bos-loofbos".geom) as geometrie, 
+    0  as relatievehoogteligging, 
+ 	 	'kbk10' as bron, 
+ 	 	13  as minzoom, 
+ 	 	15 as maxzoom
+  FROM kbk10."TRN_bos-loofbos"
+  UNION 
+  SELECT
+		"TRN_bos-naaldbos".ogc_fid::text || 'TRN_bos-naaldbos_kbk10' as identificatie_lokaal_id,
+    'bos-naaldbos' as type,
+		ST_makeValid(    "TRN_bos-naaldbos".geom) as geometrie, 
+    0  as relatievehoogteligging, 
+ 	 	'kbk10' as bron, 
+ 	 	13  as minzoom, 
+ 	 	15 as maxzoom
+  FROM kbk10."TRN_bos-naaldbos"
+  UNION 
+  SELECT
+		"TRN_bos-gemengd_bos".ogc_fid::text || 'TRN_bos-gemengd_bos_kbk10' as identificatie_lokaal_id,
+    'bos-gemengd_bos' as type,
+		ST_makeValid(    "TRN_bos-gemengd_bos".geom) as geometrie, 
+    0  as relatievehoogteligging, 
+ 	 	'kbk10' as bron, 
+ 	 	13  as minzoom, 
+ 	 	15 as maxzoom
+  FROM kbk10."TRN_bos-gemengd_bos"
+  UNION 
+  SELECT
+		"TRN_bos-griend".ogc_fid::text || 'TRN_bos-griend_kbk10' as identificatie_lokaal_id,
+    'bos-griend' as type,
+		ST_makeValid(    "TRN_bos-griend".geom) as geometrie, 
+    0  as relatievehoogteligging, 
+ 	 	'kbk10' as bron, 
+ 	 	13  as minzoom, 
+ 	 	15 as maxzoom
+  FROM kbk10."TRN_bos-griend"
+  UNION 
+  SELECT
+		"TRN_boomgaard".ogc_fid::text || 'TRN_boomgaard_kbk10' as identificatie_lokaal_id,
+    'boomgaard' as type,
+		ST_makeValid(    "TRN_boomgaard".geom) as geometrie, 
+    0  as relatievehoogteligging, 
+ 	 	'kbk10' as bron, 
+ 	 	13  as minzoom, 
+ 	 	15 as maxzoom
+  FROM kbk10."TRN_boomgaard"
+  UNION 
+  SELECT
+		"TRN_boomkwekerij".ogc_fid::text || 'TRN_boomkwekerij_kbk10' as identificatie_lokaal_id,
+    'boomkwekerij' as type,
+		ST_makeValid(    "TRN_boomkwekerij".geom) as geometrie, 
+    0  as relatievehoogteligging, 
+ 	 	'kbk10' as bron, 
+ 	 	13  as minzoom, 
+ 	 	15 as maxzoom
+  FROM kbk10."TRN_boomkwekerij"
+  UNION 
+  SELECT
+		"TRN_dodenakker".ogc_fid::text || 'TRN_dodenakker_kbk10' as identificatie_lokaal_id,
+    'dodenakker' as type,
+		ST_makeValid(    "TRN_dodenakker".geom) as geometrie, 
+    0  as relatievehoogteligging, 
+ 	 	'kbk10' as bron, 
+ 	 	13  as minzoom, 
+ 	 	15 as maxzoom
+  FROM kbk10."TRN_dodenakker"
+  UNION 
+  SELECT
+		"TRN_dodenakker_met_bos".ogc_fid::text || 'TRN_dodenakker_met_bos_kbk10' as identificatie_lokaal_id,
+    'dodenakker_met_bos' as type,
+		ST_makeValid(    "TRN_dodenakker_met_bos".geom) as geometrie, 
+    0  as relatievehoogteligging, 
+ 	 	'kbk10' as bron, 
+ 	 	13  as minzoom, 
+ 	 	15 as maxzoom
+  FROM kbk10."TRN_dodenakker_met_bos"
+  UNION 
+  SELECT
+		"TRN_fruitkwekerij".ogc_fid::text || 'TRN_fruitkwekerij_kbk10' as identificatie_lokaal_id,
+    'fruitkwekeri' as type,
+		ST_makeValid(    "TRN_fruitkwekerij".geom) as geometrie, 
+    0  as relatievehoogteligging, 
+ 	 	'kbk10' as bron, 
+ 	 	13  as minzoom, 
+ 	 	15 as maxzoom
+  FROM kbk10."TRN_fruitkwekerij"
+  UNION
+  SELECT
+		"TRN_binnentuin".ogc_fid::text || 'TRN_binnentuin_kbk10' as identificatie_lokaal_id,
+    'binnentuin' as type,
+		ST_makeValid(    "TRN_binnentuin".geom) as geometrie, 
+    0  as relatievehoogteligging, 
+ 	 	'kbk10' as bron, 
+ 	 	13  as minzoom, 
+ 	 	15 as maxzoom
+  FROM kbk10."TRN_binnentuin"
+UNION 
+ SELECT
+		"TRN_agrarisch".ogc_fid::text || 'TRN_agrarisch_kbk50' as identificatie_lokaal_id,
+    'TRN_agrarisch' as type,
+		ST_makeValid(    "TRN_agrarisch".geom) as geometrie, 
+    0  as relatievehoogteligging, 
+ 	 	'kbk50' as bron, 
+ 	 	12  as minzoom, 
+ 	 	8  as maxzoom
+  FROM kbk50."TRN_agrarisch"
+ UNION 
+ SELECT
+		"TRN_overig".ogc_fid::text || 'TRN_overig_kbk50' as identificatie_lokaal_id,
+    'overig' as type,
+		ST_makeValid(    "TRN_overig".geom) as geometrie, 
+    0  as relatievehoogteligging, 
+ 	 	'kbk50' as bron, 
+ 	 	12  as minzoom, 
+ 	 	8  as maxzoom
+  FROM kbk50."TRN_overig"
+ UNION 
+ SELECT
+		"TRN_bedrijfsterrein_dienstverlening".ogc_fid::text || 'TRN_bedrijfsterrein_dienstverlening_kbk50' as identificatie_lokaal_id,
+    'bedrijfsterrein_dienstverlening' as type,
+		ST_makeValid(    "TRN_bedrijfsterrein_dienstverlening".geom) as geometrie, 
+    0  as relatievehoogteligging, 
+ 	 	'kbk50' as bron, 
+ 	 	12  as minzoom, 
+ 	 	8  as maxzoom
+  FROM kbk50."TRN_bedrijfsterrein_dienstverlening"
+ UNION 
+ SELECT
+		"TRN_bos_groen_sport".ogc_fid::text || 'TRN_bos_groen_sport_kbk50' as identificatie_lokaal_id,
+    'bos_groen_sport' as type,
+		ST_makeValid(    "TRN_bos_groen_sport".geom) as geometrie, 
+    0  as relatievehoogteligging, 
+ 	 	'kbk50' as bron, 
+ 	 	12  as minzoom, 
+ 	 	8  as maxzoom
+  FROM kbk50."TRN_bos_groen_sport"
+ UNION 
+ SELECT
+		"TRN_zand".ogc_fid::text || 'TRN_zand_kbk50' as identificatie_lokaal_id,
+    'zand' as type,
+		ST_makeValid(    "TRN_zand".geom) as geometrie, 
+    0  as relatievehoogteligging, 
+ 	 	'kbk50' as bron, 
+ 	 	12  as minzoom, 
+ 	 	8  as maxzoom
+  FROM kbk50."TRN_zand"
+
+
 WITH DATA;
+
+
 
 -- View indexes:
 CREATE INDEX bgt_vw_terreindeel_vlak_geom_idx ON bgt.bgt_vw_terreindeel_vlak USING gist (geometrie);
